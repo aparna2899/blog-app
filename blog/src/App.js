82ca74d1df124f96dde7cabd59c04b9cb673f677
Aparna Tiwari <73102734+aparna2899@users.noreply.github.com>
2022-09-15 22:25:43 +0530

    Add Hero Component

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <Header />
+        <Hero />
       </BrowserRouter>
     );
   }
