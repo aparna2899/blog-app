6f85dc080582353525a6a6b7b7b6aa3615a08cdd
Aparna Tiwari <73102734+aparna2899@users.noreply.github.com>
2022-09-20 16:20:14 +0530

    Add functionality to tag cloud

--- blog/src/components/Tag.js
+++ blog/src/components/Tag.js
@@ -1,7 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 function Tag(props) {
-  let { tags } = props;
+  const [tags, setTags] = useState([]);
+  useEffect(() => {
+    const getTags = async () => {
+      try {
+        const res = await axios.get(
+          `https://mighty-oasis-08080.herokuapp.com/api/tags`
+        );
+        setTags(res.data.tags);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getTags();
+  }, []);
+
   return (
     <aside className="w-3/12 mt-8 bg-gray-100 p-2  h-fit">
       <p className="my-2">Popular Tags</p>
@@ -10,6 +26,7 @@ function Tag(props) {
           <li
             key={tag}
             className="bg-gray-400 m-1 inline-block py-1 px-3 rounded-2xl text-xs uppercase text-white"
+            onClick={() => props.selectTag(tag)}
           >
             <Link> {tag}</Link>
           </li>
--- blog/src/pages/Home.js
+++ blog/src/pages/Home.js
@@ -5,22 +5,35 @@ import Tag from '../components/Tag';
 
 function Home() {
   const [articles, setArticles] = useState([]);
+  const [selectedTag, setSelectedTag] = useState('');
+
   useEffect(() => {
     axios
       .get(`https://mighty-oasis-08080.herokuapp.com/api/articles`)
       .then((res) => setArticles(res.data.articles));
   }, []);
 
+  useEffect(() => {
+    const url = `https://mighty-oasis-08080.herokuapp.com/api/articles?tag=${selectedTag}`;
+    const getArticle = async () => {
+      try {
+        const res = await axios.get(url);
+        setArticles(res.data.articles);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getArticle();
+  }, [selectedTag]);
+
+  const selectTag = (tag) => {
+    setSelectedTag(tag);
+  };
+
   if (!articles) {
     return <p>Loading</p>;
   }
   articles.splice(16, 1);
-  let tags = [];
-  for (let i = 0; i < articles.length; i++) {
-    if (!tags.includes(...articles[i].tagList))
-      tags.push(...articles[i].tagList);
-  }
-
   return (
     <>
       <div className="bg-green text-white text-center py-12 shadow-[inset_0_-5px_8px_4px_rgba(0,0,0,0.2)]">
@@ -30,8 +43,12 @@ function Home() {
         <p className="text-2xl font-light">A place to share your knowledge.</p>
       </div>
       <div className="container mx-auto px-24 flex justify-between">
-        <Main articles={articles} />
-        <Tag tags={tags} />
+        <Main
+          articles={articles}
+          selectTag={selectTag}
+          selectedTag={selectedTag}
+        />
+        <Tag selectTag={selectTag} />
       </div>
     </>
   );
