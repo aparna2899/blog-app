50b18da9930b886a96de74905235b76dbfde6cf3
Aparna Tiwari <73102734+aparna2899@users.noreply.github.com>
2022-09-22 14:38:14 +0530

    make new article page functional

--- blog/src/pages/CreateArticle.js
+++ blog/src/pages/CreateArticle.js
@@ -1,12 +1,55 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
+const token = cookies.get('TOKEN');
+
 function CreateArticle() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [body, setBody] = useState('');
+  const [tagList, setTagList] = useState('');
+  const [article, setArticle] = useState({});
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const createArticle = async () => {
+      try {
+        const res = await axios({
+          method: 'POST',
+          url: `https://mighty-oasis-08080.herokuapp.com/api/articles`,
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+          data: JSON.stringify({
+            article: {
+              title,
+              description,
+              body,
+              tagList,
+            },
+          }),
+        });
+        setArticle(res.data.article);
+        window.location.href = `/articles/${article.slug}`;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    createArticle();
+  };
+
   return (
-    <form className="my-6 w-2/6 mx-auto">
+    <form className="my-6 w-2/6 mx-auto" onSubmit={(e) => handleSubmit(e)}>
       <input
         type="text"
         id="title"
         name="title"
         placeholder="Title"
         className="text-lg border block mx-auto px-5 py-2 mb-4 w-full rounded"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
       <input
         type="text"
@@ -14,12 +57,16 @@ function CreateArticle() {
         name="description"
         placeholder="Description"
         className="text-lg border block mx-auto px-5 py-2 mb-4 w-full rounded"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
       />
       <textarea
         placeholder="Write your article"
         id="body"
         name="body"
         className="text-lg border block mx-auto px-5 py-2 mb-4 w-full rounded"
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
       />
       <input
         type="text"
@@ -27,8 +74,13 @@ function CreateArticle() {
         name="tagList"
         placeholder="Enter Tags"
         className="text-lg border block mx-auto px-5 py-2 mb-4 w-full rounded"
+        value={tagList}
+        onChange={(e) => setTagList(e.target.value)}
       />
-      <button className=" bg-green hover:bg-green-dark text-white text-lg px-6 py-2 rounded block mx-auto">
+      <button
+        className=" bg-green hover:bg-green-dark text-white text-lg px-6 py-2 rounded block mx-auto"
+        onClick={(e) => handleSubmit(e)}
+      >
         Publish Article
       </button>
     </form>
